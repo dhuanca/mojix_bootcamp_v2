0f5ccb590074863c31fae449e3ccb8173ae965d5
DANNY HUNCA <dhls7@yahoo.com>
2022-08-11 20:25:57 -0400

    Creating my first commit for my app3

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -37,5 +37,11 @@ st.map(filtered_data)
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 
-st.subheader('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.'+
+st.markdown('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.'+
 'In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
+
+st.subheader('1. Walrus operator')
+
+st.markdown('The **Walrus or :=** operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+
+
