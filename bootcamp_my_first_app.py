301501b73b5140322fbfdf0f1b5baec65907c2e4
DANNY HUNCA <dhls7@yahoo.com>
2022-08-11 20:20:28 -0400

    Creating my first commit for my app2

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -34,3 +34,8 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
+
+st.subheader('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.'+
+'In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
